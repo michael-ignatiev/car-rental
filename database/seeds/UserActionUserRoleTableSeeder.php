886f52368723d50e054c9fa49bbd554326840e9b
michael <michael.ignatiev.1989@gmail.com>
2018-05-22 14:19:29 +0300

    updated seeder

--- database/seeds/UserActionUserRoleTableSeeder.php
+++ database/seeds/UserActionUserRoleTableSeeder.php
@@ -39,5 +39,89 @@ class UserActionUserRoleTableSeeder extends Seeder
             'user_action_id' => 5,
             'user_role_id' => 2,
         ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 6,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 7,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 7,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 8,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 9,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 9,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 10,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 10,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 8,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 11,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 12,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 13,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 14,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 14,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 15,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 16,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 16,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 17,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 17,
+            'user_role_id' => 2,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 18,
+            'user_role_id' => 1,
+        ]);
+        \App\UserActionUserRole::create([
+            'user_action_id' => 18,
+            'user_role_id' => 2,
+        ]);
     }
 }
